5d9ea43afa5d17b27f76a7954137d596c4776e43
Harrby <hjb9987@gmail.com>
2025-03-24 15:03:38 +0000

    moved program navigation to mainWindow
    now main window has all instances of classes. So we have one master widget to control all program navigation. (makes it much easier as program gets more complex)

--- Welcome.py
+++ Welcome.py
@@ -3,7 +3,9 @@ import os
 from PySide6 import QtGui, QtCore, QtWidgets
 from OpeningWidget import OpeningWidget
 
+
 class Welcome(QtWidgets.QWidget):
+    UserClicked = QtCore.Signal()       # define a signal to send to main when user clicks
 
     def __init__(self, name="James"):
         super().__init__()
@@ -92,17 +94,7 @@ class Welcome(QtWidgets.QWidget):
 
     def mousePressEvent(self, event):
         if event.button() == QtCore.Qt.LeftButton:
-            self.open_opening_widget()
-
-    def open_opening_widget(self):
-        self.opening_window = OpeningWidget()
-        if self.isFullScreen():
-            self.opening_window.showFullScreen()
-        elif self.isMaximized():
-            self.opening_window.showMaximized()
-        else:
-            self.opening_window.show()
-        self.close()
+            self.UserClicked.emit()
 
     def paintEvent(self, event):
         if not self.background_pixmap.isNull():
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from PySide6 import QtGui, QtWidgets
 import sys
 from PySide6.QtWidgets import QApplication
 from Welcome import Welcome
+from OpeningWidget import OpeningWidget
+
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, app: QtWidgets.QApplication):
@@ -11,8 +13,26 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.load_fonts()
 
+        self.welcome_window = Welcome()
+        self.welcome_window.UserClicked.connect(self.user_clicked_welcome_window)  # connect UserClicked signal->method
+
+        self.opening_widget = OpeningWidget()
+        self.opening_widget.hide()
+
+        self.setCentralWidget(self.welcome_window)
+
         self.setWindowTitle("Glow")
-        self.setMinimumSize(600, 400)
+
+    def user_clicked_welcome_window(self):
+        # hide first window, then set opening window to central widget and show it.
+        self.welcome_window.hide()
+        self.setCentralWidget(self.opening_widget)
+        if self.isFullScreen():
+            self.opening_widget.showFullScreen()
+        elif self.isMaximized():
+            self.opening_widget.showMaximized()
+        self.opening_widget.show()
+
 
     @staticmethod
     def load_fonts():
@@ -23,6 +43,6 @@ class MainWindow(QtWidgets.QMainWindow):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    welcome_window = Welcome()
-    welcome_window.show()
+    main_window = MainWindow(app)
+    main_window.show()
     sys.exit(app.exec())
